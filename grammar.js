808f78ef6412142eb4dc211b112cadd9b61023e4
Claude Warren <claude.warren@instaclustr.com>
2022-05-16 09:47:25 +0100

    removed commented code

--- grammar.js
+++ grammar.js
@@ -25,8 +25,6 @@ const
 
     string_str =  seq(squote, field("content", /([^']|(\'\'))*/), squote),
 
-    ///\'[a-zA-Z]([A-Za-z0-9_$]|(\'\'))*\'/,
-
     uuid_str = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/,
     hex_digit = /[0-9a-fA-F]/,
     decimal_digits = /[0-9]+/,
